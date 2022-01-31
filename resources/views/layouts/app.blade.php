edda62a121fc7e3e07f546bfee7cb572fc737969
Iloba <scarlettsney@gmail.com>
2022-01-31 19:09:30 +0100

    fixed-title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Rooah! Guild - Axie Infinity Scholarship</title>
+    <title>Join CryptoPiggy Guild - Axie Infinity Scholarship</title>
     <link rel="icon" type="image/x-icon" href="img/cp-icon.png">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
